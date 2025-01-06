e8ad513a268c01950ce0d3c3060eb6f07504413e
atul0786 <112048540+atul0786@users.noreply.github.com>
2025-01-06 22:55:09 +0530

    Update serializers.py

--- transporter_app/serializers.py
+++ transporter_app/serializers.py
@@ -10,3 +10,18 @@ class TransporterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transporter
         fields = '__all__'  # or specify the fields you want to include
+
+
+from rest_framework import serializers
+from .models import DDMDetails, DDMSummary
+
+class DDMDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DDMDetails
+        fields = '__all__'
+
+
+class DDMSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DDMSummary
+        fields = '__all__'
