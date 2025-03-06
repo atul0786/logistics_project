5acb544bcdbd56aacd9d7329a9164c2ea84b7ef7
atul0786 <112048540+atul0786@users.noreply.github.com>
2025-03-06 07:50:09 +0530

    Update dealer_filters.py

--- dealer_app/templatetags/dealer_filters.py
+++ dealer_app/templatetags/dealer_filters.py
@@ -1,1 +1,9 @@
 
+from django import template
+
+register = template.Library()
+
+@register.filter(name='get')
+def get(dictionary, key):
+    """Dictionary me se key ka value return karega"""
+    return dictionary.get(key, "N/A") if isinstance(dictionary, dict) else "N/A"
